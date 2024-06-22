9d3872025d99d3b571b802cd5b82ed3a27ebf12f
Robotex <ivanivan1001@bk.ru>
2024-06-22 09:03:17 +0300

    Initial commit

---
+++ src/GameSavingLoader.js
@@ -0,0 +1,14 @@
+import read from './reader';
+import json from './parser';
+import GameSaving from './index';
+
+export default class GameSavingLoader {
+  static load() {
+    return read()
+      .then((data) => json(data))
+      .then((value) => Object.assign(new GameSaving(), JSON.parse(value)));
+  }
+}
+
+GameSavingLoader.load()
+  .then((value) => value);
